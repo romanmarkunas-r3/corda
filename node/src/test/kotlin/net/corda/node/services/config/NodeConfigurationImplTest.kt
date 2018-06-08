015363d48679dee56185e9d5790f116ea0195a8b
Katelyn Baker <katelyn.baker@r3.com>
2018-06-08 13:22:41 +0100

    test fixes

--- node/src/test/kotlin/net/corda/node/services/config/NodeConfigurationImplTest.kt
+++ node/src/test/kotlin/net/corda/node/services/config/NodeConfigurationImplTest.kt
@@ -4,13 +4,16 @@ import net.corda.core.internal.div
 import net.corda.core.utilities.NetworkHostAndPort
 import net.corda.testing.core.ALICE_NAME
 import net.corda.testing.node.MockServices.Companion.makeTestDataSourceProperties
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
-import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.net.URI
+import java.net.URL
 import java.nio.file.Paths
 import java.util.*
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class NodeConfigurationImplTest {
@@ -32,71 +35,6 @@ class NodeConfigurationImplTest {
     }
 
     @Test
-    fun `check crashShell flags helper`() {
-        assertFalse { testConfiguration.copy(sshd = null).shouldStartSSHDaemon() }
-        assertTrue { testConfiguration.copy(sshd = SSHDConfiguration(1234)).shouldStartSSHDaemon() }
-        assertFalse { testConfiguration.copy(noLocalShell = true).shouldStartLocalShell() }
-        assertFalse { testConfiguration.copy(noLocalShell = false, devMode = false).shouldStartLocalShell() }
-        assertFalse { testConfiguration.copy(noLocalShell = false, devMode = true).shouldStartLocalShell() }
-        assertFalse { testConfiguration.copy(noLocalShell = true).shouldInitCrashShell() }
-        assertFalse { testConfiguration.copy(sshd = null).shouldInitCrashShell() }
-        assertFalse { testConfiguration.copy(noLocalShell = true, sshd = null).shouldInitCrashShell() }
-    }
-
-    @Test
-    fun `Dev mode is autodetected correctly`() {
-        val os = System.getProperty("os.name")
-
-        setSystemOs("Windows 98")
-        assertTrue(getConfig("test-config-empty.conf").getBoolean("devMode"))
-
-        setSystemOs("Mac Sierra")
-        assertTrue(getConfig("test-config-empty.conf").getBoolean("devMode"))
-
-        setSystemOs("Windows server 2008")
-        assertFalse(getConfig("test-config-empty.conf").getBoolean("devMode"))
-
-        setSystemOs("Linux")
-        assertFalse(getConfig("test-config-empty.conf").getBoolean("devMode"))
-
-        setSystemOs(os)
-    }
-
-    private fun setSystemOs(os: String) {
-        System.setProperty("os.name", os)
-    }
-
-    @Test
-    fun `Dev mode is read from the config over the autodetect logic`() {
-        assertTrue(getConfig("test-config-DevMode.conf").getBoolean("devMode"))
-        assertFalse(getConfig("test-config-noDevMode.conf").getBoolean("devMode"))
-    }
-
-    @Test
-    fun `Dev mode is true if overriden`() {
-        assertTrue(getConfig("test-config-DevMode.conf", ConfigFactory.parseMap(mapOf("devMode" to true))).getBoolean("devMode"))
-        assertTrue(getConfig("test-config-noDevMode.conf", ConfigFactory.parseMap(mapOf("devMode" to true))).getBoolean("devMode"))
-        assertTrue(getConfig("test-config-empty.conf", ConfigFactory.parseMap(mapOf("devMode" to true))).getBoolean("devMode"))
-    }
-
-    @Test
-    fun `Dev mode is false if overriden`() {
-        assertFalse(getConfig("test-config-DevMode.conf", ConfigFactory.parseMap(mapOf("devMode" to false))).getBoolean("devMode"))
-        assertFalse(getConfig("test-config-noDevMode.conf", ConfigFactory.parseMap(mapOf("devMode" to false))).getBoolean("devMode"))
-        assertFalse(getConfig("test-config-empty.conf", ConfigFactory.parseMap(mapOf("devMode" to false))).getBoolean("devMode"))
-    }
-
-    private fun getConfig(cfgName: String, overrides: Config = ConfigFactory.empty()): Config {
-        val path = this::class.java.classLoader.getResource(cfgName).toPath()
-        val cfg = ConfigHelper.loadConfig(
-                baseDirectory = path.parent,
-                configFile = path,
-                configOverrides = overrides
-        )
-        return cfg
-    }
-
-    @Test
     fun `validation has error when compatibilityZoneURL is present and devMode is true`() {
         val configuration = testConfiguration.copy(
                 devMode = true,
@@ -105,18 +43,6 @@ class NodeConfigurationImplTest {
         val errors = configuration.validate()
 
         assertThat(errors).hasOnlyOneElementSatisfying { error -> error.contains("compatibilityZoneURL") && error.contains("devMode") }
-    }
-
-    @Test
-    fun `errors for nested config keys contain path`() {
-        var rawConfig = ConfigFactory.parseResources("working-config.conf", ConfigParseOptions.defaults().setAllowMissing(false))
-        val missingPropertyPath = "rpcSettings.address"
-        rawConfig = rawConfig.withoutPath(missingPropertyPath)
-
-        assertThatThrownBy { rawConfig.parseAsNodeConfiguration() }.isInstanceOfSatisfying(ConfigException.Missing::class.java) { exception ->
-            assertThat(exception.message).isNotNull()
-            assertThat(exception.message).contains(missingPropertyPath)
-        }
     }
 
     @Test
@@ -144,15 +70,6 @@ class NodeConfigurationImplTest {
         assertThat(errors).hasOnlyOneElementSatisfying {
             error -> error.contains("Cannot configure both compatibilityZoneUrl and networkServices simultaneously")
         }
-    }
-
-    @Test
-    fun `rpcAddress and rpcSettings_address are equivalent`() {
-        var rawConfig = ConfigFactory.parseResources("working-config.conf", ConfigParseOptions.defaults().setAllowMissing(false))
-        rawConfig = rawConfig.withoutPath("rpcSettings.address")
-        rawConfig = rawConfig.withValue("rpcAddress", ConfigValueFactory.fromAnyRef("localhost:4444"))
-
-        assertThatCode { rawConfig.parseAsNodeConfiguration() }.doesNotThrowAnyException()
     }
 
     @Test
